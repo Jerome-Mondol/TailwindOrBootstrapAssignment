1678a260533e14769b7cc574e6ce767d8cfce3d7
Jerome-Mondol <jeromemondol11@gmail.com>
2024-11-12 18:18:13 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
